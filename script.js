90c5a58682da953e9c7eaa35d7a45e024d3ed5a1
Obie Munoz <obiemunozjr@gmail.com>
2021-10-08 15:36:13 -0400

    Added date; updated grid layout

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-todoListTable = document.querySelector("#todo-list-table")
+todoListTable = document.querySelector("#todo-list-table");
+today = new Date();
+date = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+document.querySelector("#date").innerText = date;
 
 function newItemToDo() {
-    newEntryText = document.querySelector("#new-item").value;
+    let newEntryText = document.querySelector("#new-item").value;
     if (newEntryText != "") {
         if (todoListTable.children.length == 0) {
             count = 1;
         } else {
             count = todoListTable.children.length + 1;
         }
-        newRow = document.createElement("tr");
-        newEntry = document.createElement("td");
+        let newRow = document.createElement("tr");
+        let newEntry = document.createElement("td");
         newEntry.innerText = count + ". " + newEntryText;
-        newEntryText = "";
         newRow.append(newEntry);
         todoListTable.append(newRow);
     } else {
         alert("You need to enter some data!")
     }
+    document.querySelector("#new-item").value = "";
 }
 
 function clearTable() {
