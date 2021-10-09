72111ab41af5376abf1ad11877b5da4216f665a0
Obie Munoz <obiemunozjr@gmail.com>
2021-10-09 10:42:21 -0400

    Updated table to list, added functionality to complete tasks.

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-todoListTable = document.querySelector("#todo-list-table");
-today = new Date();
-date = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+const todoList = document.querySelector("#todo-list");
+const finishedList = document.querySelector("#finished-list")
+
+const today = new Date();
+const date = `${today.getFullYear()}-${today.getMonth()}-${today.getDate()}`
+
 document.querySelector("#date").innerText = date;
 
 function newItemToDo() {
     let newEntryText = document.querySelector("#new-item").value;
+
     if (newEntryText != "") {
-        if (todoListTable.children.length == 0) {
-            count = 1;
-        } else {
-            count = todoListTable.children.length + 1;
-        }
-        let newRow = document.createElement("tr");
-        let newEntry = document.createElement("td");
-        newEntry.innerText = count + ". " + newEntryText;
-        newRow.append(newEntry);
-        todoListTable.append(newRow);
+        const toDoLi = document.createElement("li");
+
+        toDoLi.innerText = newEntryText;
+        toDoLi.class = "todo";
+        toDoLi.addEventListener("click", handleDelete)
+        console.log(toDoLi)
+
+        todoList.append(toDoLi)
     } else {
         alert("You need to enter some data!")
     }
     document.querySelector("#new-item").value = "";
 }
 
+function handleDelete(event) {
+    console.log(event.target);
+    finishedList.append(event.target);
+    event.target.removeEventListener("click", handleDelete)
+}
+
 function clearTable() {
-    while (todoListTable.firstChild) {
-        todoListTable.removeChild(todoListTable.lastChild)
-    }
+    todoList.innerHTML = "";
 }
 
 document.querySelector("#submit").addEventListener("click", newItemToDo);
