1f78dc14b938cc01ddaf781fa583bfeae7eb2ab8
Obie Munoz <obiemunozjr@gmail.com>
2021-10-09 12:03:15 -0400

    Add footer to page via js

--- script.js
+++ script.js
@@ -43,3 +43,13 @@ document.querySelector("#new-item").addEventListener("keyup", function(event) {
         newItemToDo();
     }
 })
+
+// CREATE A FOOTER FOR OUR PAGES
+const thisYear = new Date().getFullYear();
+const footer = document.createElement("div");
+const footerLink = document.createElement("a")
+footer.className = "footer"
+footerLink.href = "mailto:obiemunozjr@gmail.com"
+footerLink.innerText = `Copyright © ${thisYear} Obie Munoz`
+footer.append(footerLink)
+document.body.querySelector(".container").append(footer)
